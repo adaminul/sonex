d7c3a559588f10f3c490ccd7f82a8c7c07b46706
beksbhale20 <61486131+beksbhale20@users.noreply.github.com>
2020-04-08 19:15:58 +0700

    Add files via upload

--- cfg.php
+++ cfg.php
@@ -1,15 +1,15 @@
 <?php
 
-// ENTER DATA COOKIE
+// MASUKAN DATA COOKIE KALIAN
+// ENTER YOUR DATA COOKIE
 
-$__cfduid = "__cfduid=d66a2dfe1fd10e518f6ae09c536d7853c1586311655";
+$__cfduid = "d056229272f491b3d265ce442961fb3981585847946";
 
-$__tawkuuid = "__tawkuuid=e::sonex.cloud::77M/PrpVym7sitOPvJBfwvMPPD07dKLSBKkhDmr75mcc2/BDzrdRPLEaUyl/E7ma::2";
+$__tawkuuid = "e::bitland.cash::qfEfaqLSW6o/EMvbOtZ2PBjW0/fgP+IFBBQ6I+IGyzZqqD05va6xCe6kg7WlH6fa::2";
 
-$_gid = "_gid=GA1.2.1060411138.1586311660";
+// MASUKAN DATA LOGIN KALIAN
+// ENTER YOUR DATA LOGIN
 
-// ENTER DATA LOGIN
+$email = "uberku563@gmail.com";
 
-$email = "adammaulana2506@gmail.com";
-
-$password = "gitafitri";
+$password = "bhaleku19";
